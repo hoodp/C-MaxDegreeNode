dcb0a5a29a8b68880ae79e043ba91ccffab784b7
Paul Hood <hoodp@mail.gvsu.edu>
2015-08-01 14:07:53 -0400

    updated program to find the max degree from an adjacency matrix

--- maxDegree.c
+++ maxDegree.c
@@ -1,8 +1,38 @@
-#include <stdlib.h>
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-  printf("Hello World!!\n");
+
+  // stores the maximum degree value
+  int max = 0;
+
+  // get the size of the matrix 
+  int nodes;
+  scanf("%d", &nodes);
+  printf("Size: %d\n", nodes);
+
+  // starting looping through each row
+  int i, j;
+  for (i = 0; i < nodes; i++) {
+    printf("%d: ", i);
+    int edges = 0;
+    // start looping through each column
+    for (j = 0; j < nodes; j++) {
+      int value;
+      scanf("%d", &value);
+      printf("%d ", value);
+      if (i != j && value == 1) {
+	edges++;
+	/*int value;
+	scanf("%d", &value);
+	printf("%d ", value);*/
+	
+      }
+    }
+    if (edges > max) 
+      max = edges;
+    printf(": %d\n", max);
+  }
+  printf("Max: %d\n", max);
   return 0;
 }
